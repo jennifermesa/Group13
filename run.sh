e14617f64e3fd9e19606f083888d779689fa4131
CarlosMelicandia <c.melicandia15@gmail.com>
2026-02-15 21:26:23 -0500

    added script to quick run app

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# If you cant run this do: chmod +x run.sh
+
+# Create virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv venv
+fi
+
+# Activate virtual environment
+echo "Activating virtual environment..."
+source venv/bin/activate
+
+# Install requirements
+echo "Installing requirements..."
+pip install -r requirements.txt
+
+# Run server
+echo "Starting Django server..."
+python manage.py runserver
