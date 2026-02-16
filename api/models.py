f98b2ea0912d77a19828c7ad71b2895e0aed2031
CarlosMelicandia <c.melicandia15@gmail.com>
2026-02-15 21:09:55 -0500

    added email field to model

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=128)
+    email = models.CharField(max_length = 128)
 
     def __str__(self):
         return self.username
